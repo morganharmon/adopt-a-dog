16c4cce4fe54a04892f872508523f09857755042
Morgan Harmon <morganharmon@gmail.com>
2022-05-11 12:09:25 -0700

    render main page dogs

--- app.js
+++ app.js
@@ -6,3 +6,12 @@ const dogListContainer = document.getElementById('dog-list-container');
 // on load
 // fetch all dogs
 // render and append all dog cards to the container
+
+async function loadData() {
+    const doggies = await getDogs();
+    for (let dog of doggies) {
+        dogListContainer.append(renderDogCard(dog));
+    }
+}
+
+loadData();
